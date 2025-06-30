9cc4bd97b788c9e78e1374a1f7297eb300ce8762
Aayan Mumtaz <161371453+Not-Secret-Dev@users.noreply.github.com>
2025-06-30 20:25:52 +0500

    Updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,8 +28,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    credentials: true
   })
 );
 
